fd7ff3415b6480e6ad53daf83363fd4bf9c4f484
sherwan <sherwanuodcs@gmail.com>
2026-01-25 15:35:32 +0100

    created registration-form

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Enum\Gender;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class, [ 
+                'attr' => [
+                    'placeholder' => 'First name',
+                ],
+                'required' => true,
+            ])
+            ->add('lastName', TextType::class, [ 
+                'attr' => [
+                    'placeholder' => 'Last name',
+                ],
+                'required' => true,
+            ])
+            ->add('gender', ChoiceType::class, [
+                'choices' => [
+                    'Male' => Gender::MALE,
+                    'Female' => Gender::FEMALE,
+                    'Diverse' => Gender::DIVERSE,
+                ],
+                'expanded' => true,   // radios instead of <select>
+                'multiple' => false,  // one choice only
+                'required' => true,
+                'label' => 'Gender',
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'placeholder' => 'Email address',
+                ],
+                'required' => true,
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue(message: 'You should agree to our terms.'),
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password', 
+                           'placeholder' => 'Password'],
+                'constraints' => [
+                    new NotBlank(
+                        message: 'Please enter a password'
+                    ),
+                    new Length(
+                        min: 6,
+                        minMessage: 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        max: 4096,
+                    ),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
