8b9eafb68d77e9a7201e32bd2b63b5b248d63751
sherwan <sherwanuodcs@gmail.com>
2026-01-26 16:52:14 +0100

    refactor password visibility toggle logic in registration form

--- assets/custom.js
+++ assets/custom.js
@@ -1,13 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const passwordInput = document.querySelector(".password-input");
-    const toggleIcon = document.getElementById("togglePasswordIcon");
+    //CB10: Toggle password visibility on registration-form by clicking the eye icon
+    const registrationPage = document.querySelector(".registration-page");
+    if (registrationPage) {
+        const passwordInput = document.querySelector(".password-input");
+        const toggleIcon = document.getElementById("togglePasswordIcon");
 
-    if (toggleIcon && passwordInput) {
-        toggleIcon.addEventListener("click", function () {
-            const isPassword = passwordInput.type === "password";
-            passwordInput.type = isPassword ? "text" : "password";
-            toggleIcon.classList.toggle("fa-eye-slash");
-            toggleIcon.classList.toggle("fa-eye");
-        });
+        if (toggleIcon && passwordInput) {
+            toggleIcon.addEventListener("click", function () {
+                const isPassword = passwordInput.type === "password";
+                passwordInput.type = isPassword ? "text" : "password";
+                toggleIcon.classList.toggle("fa-eye-slash");
+                toggleIcon.classList.toggle("fa-eye");
+            });
+        }
     }
+    // End of CB10
 });
