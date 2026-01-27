b017870ad2c6208432fdde7d88de4eca2d31838e
sherwan <sherwanuodcs@gmail.com>
2026-01-27 15:00:35 +0100

    add user redirection after successful login in LoginController

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,12 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class LoginController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils,
+        #[CurrentUser] ?User $user,): Response
     {
+        if ($user) {
+            return $this->redirectToRoute('app_home');
+        }
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
