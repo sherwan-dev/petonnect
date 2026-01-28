514ad4cc20888545e2be96a54786453aba9bab88
sherwan <sherwanuodcs@gmail.com>
2026-01-28 18:28:01 +0100

    add success flash for registration form

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -50,8 +50,8 @@ class RegistrationController extends AbstractController
             );
 
             // do anything else you need here, like send an email
-
-            return $this->redirectToRoute('app_welcome');
+            $this->addFlash('success', 'Registration successful! Please check your email to verify your account.');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
@@ -86,6 +86,6 @@ class RegistrationController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_login');
     }
 }
