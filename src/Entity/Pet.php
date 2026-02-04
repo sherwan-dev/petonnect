5c37845e411dd6afd9455f3476f7f60d7c9745df
sherwan <sherwanuodcs@gmail.com>
2026-02-04 19:12:06 +0100

    add Pet Entity

---
+++ src/Entity/Pet.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PetRepository;
+use Doctrine\ORM\Mapping as ORM;
+use App\Enum\PetGender;
+
+#[ORM\Entity(repositoryClass: PetRepository::class)]
+class Pet
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne(inversedBy: 'pets')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?PetGender $gender = null;
+
+    #[ORM\ManyToOne(inversedBy: 'pets')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $owner = null;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getGender(): ?PetGender
+    {
+        return $this->gender;
+    }
+
+    public function setGender(?PetGender $gender): static
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner): static
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+}
