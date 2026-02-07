3d749de80a9ffe1a68ec831c419cf9e46a3b8727
sherwan <sherwanuodcs@gmail.com>
2026-02-07 21:01:11 +0100

    removed pet-routs from user-controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,20 +7,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route; 
 use App\Entity\User;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use App\Form\PetType;
+use App\Entity\Pet;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
- #[Route(path: '/user', name: 'app_user')]
+ #[Route(path: '/user', name: 'app_user_')]
 class UserController extends AbstractController
 {
-    #[Route(path: '/board', name: 'app_user_board')]
+    #[Route(path: '/board', name: 'board')]
     public function boardIndex(#[CurrentUser] ?User $user): Response
     { 
-        return $this->render('user/pets.html.twig');
+        return $this->render('user/board.html.twig');
     }
-
-    #[Route(path: '/pets', name: 'app_user_pets')]
-    public function petsIndex(#[CurrentUser] ?User $user): Response
-    { 
-        return $this->render('user/pets.html.twig');
-    }
- 
 }
