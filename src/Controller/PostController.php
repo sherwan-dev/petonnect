8e45da64c55bfafdc517d17792db53312b58f1e0
sherwan <sherwanuodcs@gmail.com>
2026-02-18 21:11:05 +0100

    handling images upload

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -10,11 +10,20 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\UX\Turbo\TurboBundle;
+use App\Entity\PostImage;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use App\Service\FileUploader;
 
 class PostController extends AbstractController
 {
+    public function __construct(
+        private string $postsImagesDir,
+    ) {
+    }
+    
     #[Route('/post/new', name: 'app_post_new', methods: ['POST'])]
-    public function new(Request $request, EntityManagerInterface $em): Response
+    public function new(Request $request, EntityManagerInterface $em,
+        FileUploader $fileUploader): Response
     {
         $post = new Post();
         $form = $this->createForm(PostType::class, $post);
@@ -22,18 +31,28 @@ class PostController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $post->setAuthor($this->getUser()->getPets()->first());//TODO: Handle active pet
-            
+
+            $imageFiles = $form->get('imageFiles')->getData();
+            if ($imageFiles) {
+                foreach ($imageFiles as $imageFile) {
+                    $profilePictureFileName = $fileUploader->upload($imageFile, $this->postsImagesDir);
+                    $postImage = new PostImage();
+                    $postImage->setImageFilename($profilePictureFileName);
+                    $post->addImage($postImage);
+                }
+            }
+
             $em->persist($post);
             $em->flush();
 
             if (TurboBundle::STREAM_FORMAT === $request->getPreferredFormat()) {
                 return $this->render('post/_create_success.stream.html.twig', [
                     'post' => $post,
-                    'form' => $this->createForm(PostType::class)->createView() 
+                    'form' => $this->createForm(PostType::class)->createView()
                 ]);
             }
 
-            return $this->redirectToRoute('app_home'); 
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->render('post/_create.html.twig', [
