c071736f0a9a57692f573671ec285c947e0dafc3
sherwan <sherwanuodcs@gmail.com>
2026-03-05 16:16:05 +0100

    make toast auto closeable

--- assets/controllers/closeable_controller.js
+++ assets/controllers/closeable_controller.js
@@ -1,6 +1,16 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+    static values = {
+        autoClose: Number,
+    };
+    connect() {
+        if (this.autoCloseValue) {
+            setTimeout(() => {
+                this.close();
+            }, this.autoCloseValue);
+        }
+    }
     close() {
         this.element.remove();
     }
