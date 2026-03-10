02f4bcfb507396f60929bf336e122edc5bb03fc5
sherwan <sherwanuodcs@gmail.com>
2026-03-10 21:39:48 +0100

    toggeling border for active pet

--- assets/controllers/pet_selection_controller.js
+++ assets/controllers/pet_selection_controller.js
@@ -1,24 +1,29 @@
-import { Controller } from '@hotwired/stimulus';
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    connect() {
-        console.log('PetSelectionController connected');
-    }
+    async select() {
+        const url = this.element.dataset.actionUrl;
+        const pets = document.querySelectorAll('.pets-container .pet-item');
+        
+        pets.forEach((pet) => {
+            const isTarget = pet.dataset.petId === this.element.dataset.petId;
+            const imageContainer = pet.querySelector('.pet-image-container');
+            
+            pet.classList.toggle('active', isTarget);
+            imageContainer?.classList.toggle('ring-3', isTarget);
+            imageContainer?.classList.toggle('ring-(--green-600)', isTarget);
+        });
 
-async select() {
-    const url = this.element.dataset.actionUrl;
+        let response = await fetch(url, {
+            method: "POST",
+            headers: {
+                Accept: "text/vnd.turbo-stream.html",
+            },
+        });
 
-    let response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Accept': 'text/vnd.turbo-stream.html' 
+        if (response.ok) {
+            const html = await response.text();
+            Turbo.renderStreamMessage(html);
         }
-    });
-
-    if (response.ok) {
-        const html = await response.text();
-        Turbo.renderStreamMessage(html); 
     }
-}
-
 }
